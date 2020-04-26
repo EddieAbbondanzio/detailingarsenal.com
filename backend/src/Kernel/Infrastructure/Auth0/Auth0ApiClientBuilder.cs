0fc4b12b002738d72b446d061f2b4e3d09f7f83d
Ed Abbondanzio <me@eddieabbondanz.io>
2020-04-25 22:37:59 -0400

    Refactor: Added ability to refresh auth0 api tokens automatically

--- backend/src/Kernel/Infrastructure/Auth0/Auth0ApiClientBuilder.cs
+++ backend/src/Kernel/Infrastructure/Auth0/Auth0ApiClientBuilder.cs
@@ -7,6 +7,8 @@ using Auth0.ManagementApi;
 public class Auth0ApiClientBuilder : IAuth0ApiClientBuilder {
     private Auth0Config config;
 
+    private static Auth0ApiToken? token;
+
     public Auth0ApiClientBuilder(Auth0Config config) {
         this.config = config;
     }
@@ -18,12 +20,28 @@ public class Auth0ApiClientBuilder : IAuth0ApiClientBuilder {
     public async Task<ManagementApiClient> GetManagementApiClient() {
         var authenticationApiClient = new AuthenticationApiClient(new Uri(config.Domain));
 
-        // How to generate token: https://github.com/auth0/auth0.net/issues/171
-        var token = await authenticationApiClient.GetTokenAsync(new ClientCredentialsTokenRequest() {
-            ClientId = config.ClientId,
-            ClientSecret = config.ClientSecret,
-            Audience = $"{config.Domain}api/v2/"
-        });
+        if (token == null) {
+            // How to generate token: https://github.com/auth0/auth0.net/issues/171
+            var response = await authenticationApiClient.GetTokenAsync(new ClientCredentialsTokenRequest() {
+                ClientId = config.ClientId,
+                ClientSecret = config.ClientSecret,
+                Audience = $"{config.Domain}api/v2/"
+            });
+
+            token = new Auth0ApiToken(response);
+        }
+
+        // Refresh access token if needed
+        if (token.IsExpired()) {
+            var response = await authenticationApiClient.GetTokenAsync(new RefreshTokenRequest() {
+                ClientId = config.ClientId,
+                ClientSecret = config.ClientSecret,
+                Audience = $"{config.Domain}api/v2/",
+                RefreshToken = token.RefreshToken
+            });
+
+            token = new Auth0ApiToken(response);
+        }
 
         return new ManagementApiClient(token.AccessToken, new Uri($"{config.Domain}api/v2/"));
     }
---
+++ backend/src/Kernel/Infrastructure/Auth0/Auth0ApiToken.cs
@@ -0,0 +1,18 @@
+using System;
+using Auth0.AuthenticationApi.Models;
+
+public class Auth0ApiToken {
+    public string AccessToken { get; }
+    public string RefreshToken { get; }
+    public DateTime Expiration { get; }
+
+    public Auth0ApiToken(AccessTokenResponse response) {
+        AccessToken = response.AccessToken;
+        RefreshToken = response.RefreshToken;
+        Expiration = DateTime.Now.AddSeconds(response.ExpiresIn);
+    }
+
+    public bool IsExpired() {
+        return DateTime.UtcNow > Expiration;
+    }
+}
