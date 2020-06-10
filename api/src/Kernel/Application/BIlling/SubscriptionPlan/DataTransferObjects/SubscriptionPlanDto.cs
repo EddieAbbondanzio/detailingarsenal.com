4b36dc2741e19a45e283066fa1c7329cc46158dc
Ed Abbondanzio <me@eddieabbondanz.io>
2020-06-10 19:34:54 -0400

    Bug: Susbcription plan id was not being sent to front end

--- api/src/Kernel/Application/BIlling/SubscriptionPlan/DataTransferObjects/SubscriptionPlanDto.cs
+++ api/src/Kernel/Application/BIlling/SubscriptionPlan/DataTransferObjects/SubscriptionPlanDto.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 
 public class SubscriptionPlanDto : IDataTransferObject {
+    public Guid Id { get; set; }
+    public string ExternalId { get; set; } = null!;
     public string Name { get; set; } = null!;
     public List<SubscriptionPlanPriceDto> Prices { get; set; } = null!;
-    public string ExternalId { get; set; } = null!;
 }
--- api/src/Kernel/Application/BIlling/SubscriptionPlan/DataTransferObjects/SubscriptionPlanPriceDto.cs
+++ api/src/Kernel/Application/BIlling/SubscriptionPlan/DataTransferObjects/SubscriptionPlanPriceDto.cs
@@ -1,5 +1,8 @@
+using System;
+
 public class SubscriptionPlanPriceDto : IDataTransferObject {
+    public Guid Id { get; set; }
+    public string ExternalId { get; set; } = null!;
     public decimal Price { get; set; }
     public string Interval { get; set; } = null!;
-    public string ExternalId { get; set; } = null!;
 }
