7cd4074a14ab9c08dff63ceef7c87bf9d8a688b7
Ed Abbondanzio <me@eddieabbondanz.io>
2020-07-01 06:32:38 -0400

    Test: Added test to verify user service adds to database when creating user

---
+++ api/tests/Domain/Users/Services/UserServiceTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using DetailingArsenal.Domain;
+using DetailingArsenal.Domain.Settings;
+using DetailingArsenal.Domain.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DetailingArsenal.Tests.Domain.Users {
+    [TestClass]
+    public class UserServiceTests {
+        [TestMethod]
+        public async Task CreateUserAddsToDatabase() {
+            var eventPublisher = Mock.Of<IDomainEventPublisher>();
+            var userGateway = Mock.Of<IUserGateway>();
+            var repoMock = new Mock<IUserRepo>();
+
+            var service = new UserService(
+                userGateway,
+                repoMock.Object,
+                eventPublisher
+            );
+
+            await service.CreateUser(
+                "fake@mail.com",
+                "password"
+            );
+
+            repoMock.Verify(r => r.Add(It.IsAny<User>()), Times.Once);
+        }
+
+    }
+}
