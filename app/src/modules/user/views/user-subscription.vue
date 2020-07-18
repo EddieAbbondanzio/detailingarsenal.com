16247a00c367611d41d93d0c2ef7bd9a62451ae1
Ed Abbondanzio <me@eddieabbondanz.io>
2020-07-18 14:45:22 -0400

    Refactor: Customer subscription will now be nullable because a customer can have no subscription if it was cancelled.

--- app/src/modules/user/views/user-subscription.vue
+++ app/src/modules/user/views/user-subscription.vue
@@ -4,14 +4,20 @@
             <page-header title="Subscription" description="Subscription plan and billing">
                 <template v-slot:breadcrumb-trail>
                     <breadcrumb-trail>
-                        <breadcrumb name="Subscription" :to="{ name: 'userSubscription' }" active="true" />
+                        <breadcrumb
+                            name="Subscription"
+                            :to="{ name: 'userSubscription' }"
+                            active="true"
+                        />
                     </breadcrumb-trail>
                 </template>
             </page-header>
         </template>
 
         <div class="box is-shadowless" v-if="customer != null">
-            <p class="is-size-4 has-text-weight-bold has-text-centered has-margin-bottom-3">One plan, no confusion.</p>
+            <p
+                class="is-size-4 has-text-weight-bold has-text-centered has-margin-bottom-3"
+            >One plan, no confusion.</p>
 
             <div class="card has-margin-top-3 has-margin-bottom-4">
                 <!-- Status -->
@@ -25,9 +31,9 @@
 
                 <!-- Title -->
                 <div class="has-background-primary has-padding-all-4">
-                    <p class="is-size-4 has-text-weight-bold has-text-white has-text-centered">
-                        Detailing Arsenal {{ name }}
-                    </p>
+                    <p
+                        class="is-size-4 has-text-weight-bold has-text-white has-text-centered"
+                    >Detailing Arsenal {{ name }}</p>
                 </div>
 
                 <div class="level has-padding-all-4">
@@ -43,11 +49,16 @@
                                     <small class="is-size-3" style="vertical-align: text-top;">$</small>
                                     {{ yearPrice }}
                                 </p>
-                                <p class="is-size-5 has-text-grey is-uppercase" style="align-self: bottom">
-                                    / {{ showYearly ? 'year' : 'month' }}
-                                </p>
+                                <p
+                                    class="is-size-5 has-text-grey is-uppercase"
+                                    style="align-self: bottom"
+                                >/ {{ showYearly ? 'year' : 'month' }}</p>
 
-                                <b-switch class="has-margin-y-3" type="is-info" v-model="showYearly">
+                                <b-switch
+                                    class="has-margin-y-3"
+                                    type="is-info"
+                                    v-model="showYearly"
+                                >
                                     Yearly
                                     <small class="has-text-grey">(2 months free!)</small>
                                 </b-switch>
@@ -60,15 +71,27 @@
                         <div class="is-flex is-flex-column">
                             <ul class="has-margin-bottom-4">
                                 <li>
-                                    <b-icon icon="check" type="is-success" class="has-padding-right-3" />Unlimited
+                                    <b-icon
+                                        icon="check"
+                                        type="is-success"
+                                        class="has-padding-right-3"
+                                    />Unlimited
                                     appointment bookings
                                 </li>
                                 <li>
-                                    <b-icon icon="check" type="is-success" class="has-padding-right-3" />Unlimited
+                                    <b-icon
+                                        icon="check"
+                                        type="is-success"
+                                        class="has-padding-right-3"
+                                    />Unlimited
                                     contacts
                                 </li>
                                 <li>
-                                    <b-icon icon="check" type="is-success" class="has-padding-right-3" />Synchronized
+                                    <b-icon
+                                        icon="check"
+                                        type="is-success"
+                                        class="has-padding-right-3"
+                                    />Synchronized
                                     multi device support
                                 </li>
                             </ul>
@@ -80,18 +103,17 @@
                                     :outlined="customer.subscription.status == 'active'"
                                     :disabled="customer.subscription.status == 'active'"
                                     @click="onActionClick"
-                                    >{{ customer.subscription.status == 'active' ? 'Active!' : 'Activate' }}</b-button
-                                >
+                                >{{ customer.subscription.status == 'active' ? 'Active!' : 'Activate' }}</b-button>
 
                                 <span
                                     class="has-text-grey has-margin-left-1"
                                     v-if="customer.subscription.status == 'trialing'"
-                                    >Trial ends on {{ customer.subscription.trialEnd | date }}</span
-                                >
+                                >Trial ends on {{ customer.subscription.trialEnd | date }}</span>
 
-                                <b-button type="is-text" v-if="customer.subscription.status == 'active'"
-                                    >Cancel my subscription</b-button
-                                >
+                                <b-button
+                                    type="is-text"
+                                    v-if="customer.subscription.status == 'active'"
+                                >Cancel my subscription</b-button>
                             </div>
                         </div>
                     </div>
@@ -112,13 +134,16 @@
 
                 <div class="has-margin-bottom-3" v-if="customer.paymentMethod != null">
                     <p class="is-size-4 has-text-weight-bold">Payment Method</p>
-                    <p class="is-size-6">
-                        {{ customer.paymentMethod.brand }} ending in {{ customer.paymentMethod.last4 }}
-                    </p>
-                    <p class="is-size-6 has-text-grey" v-if="customer.subscription.nextPayment != null">
-                        Next payment on {{ customer.subscription.nextPayment | date }}
-                    </p>
-                    <b-button type="is-text">Update</b-button>
+                    <div class="is-flex is-flex-row is-align-items-center">
+                        <p
+                            class="is-size-6"
+                        >{{ customer.paymentMethod.brand }} ending in {{ customer.paymentMethod.last4 }}</p>
+                        <p
+                            class="is-size-6 has-text-grey"
+                            v-if="customer.subscription.nextPayment != null"
+                        >Next payment on {{ customer.subscription.nextPayment | date }}</p>
+                        <b-button class="has-padding-y-0 has-margin-y-0" type="is-text">Update</b-button>
+                    </div>
                 </div>
             </div>
         </div>
