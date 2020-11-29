e1b22aec03bd7f1764832f38e0654fb9a110f165
Ed <me@eddieabbondanz.io>
2020-11-29 16:35:28 -0500

    Feature: Added users submodule nav links to admin panel

--- app/src/modules/admin/core/views/admin-parent-view.vue
+++ app/src/modules/admin/core/views/admin-parent-view.vue
@@ -1,20 +1,18 @@
 <template>
     <div class="is-flex is-flex-column is-flex-grow-1">
         <navbar>
-            <navbar-item :to="{name: 'schedulingPanel'}">Scheduling</navbar-item>
-            <navbar-item :to="{name: 'productCatalogPanel'}">Product Catalog</navbar-item>
+            <navbar-item :to="{ name: 'schedulingPanel' }">Scheduling</navbar-item>
+            <navbar-item :to="{ name: 'productCatalogPanel' }">Product Catalog</navbar-item>
+            <navbar-item :to="{ name: 'usersPanel' }">Users</navbar-item>
         </navbar>
         <div class="app-content">
             <router-view></router-view>
         </div>
 
         <nav-footer>
-            <nav-footer-item :to="{name: 'schedulingPanel'}" text="Scheduling" icon="calendar" />
-            <nav-footer-item
-                :to="{name: 'productCatalogPanel'}"
-                text="Product Catalog"
-                icon="book-open"
-            />
+            <nav-footer-item :to="{ name: 'schedulingPanel' }" text="Scheduling" icon="calendar" />
+            <nav-footer-item :to="{ name: 'productCatalogPanel' }" text="Product Catalog" icon="book-open" />
+            <nav-footer-item :to="{ name: 'usersPanel' }" text="Users" icon="account" />
         </nav-footer>
     </div>
 </template>
