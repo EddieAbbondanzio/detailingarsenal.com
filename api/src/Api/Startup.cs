7df9a2a063d20cd14662b7ad07502d60826b475e
Ed <me@eddieabbondanz.io>
2021-01-22 18:03:11 -0500

    Added console log statement saying what environment api is running in

--- api/src/Api/Startup.cs
+++ api/src/Api/Startup.cs
@@ -67,6 +67,8 @@ namespace DetailingArsenal.Api {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            Console.WriteLine("Running in production: ", environment.IsProduction());
+
             services.AddCors();
 
             // Core
