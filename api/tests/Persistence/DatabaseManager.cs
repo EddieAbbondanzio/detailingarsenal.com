be3684921245208e0124a411511ae83534a598e4
Ed <me@eddieabbondanz.io>
2021-02-03 05:53:14 -0500

    Feature: Added test database and run migrations at start of tests to allow for database integration tests

---
+++ api/tests/Persistence/DatabaseManager.cs
@@ -0,0 +1,41 @@
+using System;
+using DetailingArsenal.Domain;
+using DetailingArsenal.Persistence;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DetailingArsenal.Tests.Persistence {
+    [TestClass]
+    public class DatabaseManager {
+        [AssemblyInitialize]
+        public static void Configure(TestContext context) {
+            // https://stackoverflow.com/questions/60994309/net-core-3-1-loading-config-from-appsettings-json-for-console-application
+            var c = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Tests.json")
+                .Build();
+
+            var dbConfig = new PostgresDatabaseConfig();
+            c.Bind("Database", dbConfig);
+
+
+
+            var serviceProvider = new ServiceCollection();
+            serviceProvider.AddDatabaseMigrations(dbConfig.GetConnectionString(), typeof(MigrationsFlag).Assembly);
+
+            var services = serviceProvider.BuildServiceProvider();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = services.CreateScope()) {
+                // Instantiate the runner
+                var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+                // Execute the migrations
+                runner.MigrateUp();
+            }
+
+        }
+    }
+}
---
+++ api/tests/Persistence/UnitTests.cs
@@ -0,0 +1,13 @@
+using System;
+using DetailingArsenal.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DetailingArsenal.Tests.Persistence {
+    [TestClass, TestCategory("Integration")]
+    public class UnitTests {
+        [TestMethod]
+        public void Blah() {
+            Assert.IsTrue(true);
+        }
+    }
+}
